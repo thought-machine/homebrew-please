490e94cd26ca94882ede090e0d7eb95787492be3
Peter Ebden <peter.ebden@gmail.com>
2019-03-10 09:06:36 +0000

    Add Homebrew recipe

---
+++ please.rb
@@ -0,0 +1,18 @@
+class Please < Formula
+  desc "High-performance extensible build system for reproducible builds."
+  homepage "https://please.build"
+  url "https://github.com/thought-machine/please/archive/v13.4.8.tar.gz"
+  sha256 "9d581fe6a44a902401c7f1808d33ed55564fab233cf4a64cc39ef35a5a0ad8d2"
+  depends_on "go" => :build
+
+  def install
+    system "./bootstrap.sh --skip_tests"
+    libexec.install Dir["plz-out/gen/package/*"]
+    bin.install_symlink libexec/"please"
+  end
+
+  test do
+    system "plz init"
+    system "plz build ///pleasings//go:all"
+  end
+end
