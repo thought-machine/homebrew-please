3b56f7dbbc96cfec0a7a9319b53851b82b4181fc
Peter Ebden <peter.ebden@gmail.com>
2019-03-14 21:18:21 +0000

    Update download location

--- please.rb
+++ please.rb
@@ -17,8 +17,11 @@ class Please < Formula
   end
 
   bottle do
-    root_url "https://homebrew.bintray.com/bottles-please"
+    root_url "https://github.com/thought-machine/homebrew-please/releases/download/v13.4.8/"
     cellar :any_skip_relocation
-    sha256 "fc2dbe37f068b6bce11cc524b9f0a3e315276983f5f6ef1556fafa8ade2ace8c" => :mojave
+    sha256 "2456db20e2c6b312a2ab67432c7f3d9f611be62a31b0f28431a29f090fa33988" => :el_capitan
+    sha256 "f6bd326cc6790dfd6832c2e0086251c7815d742fc8b8853b7a31be2afa75ffb6" => :yosemite
+    sha256 "4f6dbc097fab5a5560edd3ddadd2a61ef203e0c8f06eb49a25344e00907e62a2" => :mojave
+    sha256 "654d7ef0f8fa83b56586e0a5b906cd8ddc20dc38c2ec220c0802dc94e5a9d218" => :linux_x86_64
   end
 end
