27f068e4ce87157cfdbcc225e34362d252e58311
The Hogfather <the-hogfather@thoughtmachine.net>
2020-02-04 11:46:42 +0000

    Release version 14.3.2

--- please.rb
+++ please.rb
@@ -1,7 +1,7 @@
 class Please < Formula
   desc "High-performance extensible build system for reproducible builds."
   homepage "https://please.build"
-  url "https://github.com/thought-machine/please/archive/v14.3.1.tar.gz"
+  url "https://github.com/thought-machine/please/archive/v14.3.2.tar.gz"
   sha256 "6953ed196d8871ce977ff5649a6ebf40371bd699f09204560e8083ae140babf1"
   depends_on "go" => :build
 
@@ -17,11 +17,11 @@ class Please < Formula
   end
 
   bottle do
-    root_url "https://github.com/thought-machine/homebrew-please/releases/download/v14.3.1"
+    root_url "https://github.com/thought-machine/homebrew-please/releases/download/v14.3.2"
     cellar :any_skip_relocation
-    sha256 "673460d9059a35321e0e6dc381ce957b0830bcb0a7b65b722dc54c4dc161c896" => :el_capitan
-    sha256 "4f56dc5875463e8aca60b219521f62d474047a200042309d602979ea39396716" => :yosemite
-    sha256 "2cbdb278c5890624e5c58cc2f9e1913ea4475fd852f4059dfcd27fbccc1912a3" => :mojave
-    sha256 "46c446f4e40bfe31dffa2ed8818890712ac2f6d7e80486b7c29d17f527a3ea19" => :linux_x86_64
+    sha256 "ca9b2b3f6308787103380790f903683cca7013b4e66f777db22da6bdfc3f2fc1" => :el_capitan
+    sha256 "2250e0cc1eb524ba1422f742da220c8c15122d712a89531a89486d7434593bf1" => :yosemite
+    sha256 "2cac3bf096c6fa7fb3ba6f230939eae4e4520a315bfc9c7056600eb5c831c33d" => :mojave
+    sha256 "39f45bd458b001f94999ab43fe9ee2a1ad044a34e882c7e3b68575cfe1f8c4eb" => :linux_x86_64
   end
 end
