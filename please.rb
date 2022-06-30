2b28eb32c242372d017b6c448bd1608c75cf6adc
The Hogfather <the-hogfather@thoughtmachine.net>
2022-06-30 09:11:40 +0000

    Release version 16.21.3

--- please.rb
+++ please.rb
@@ -5,14 +5,14 @@
 class Please < Formula
   desc "High-performance extensible build system for reproducible builds"
   homepage "https://please.build"
-  url "https://github.com/thought-machine/please/archive/v16.21.2.tar.gz"
-  sha256 "bd79e57459304579abb57360da39b2da853339934ba8df6ddbb00611ec33aadf"
+  url "https://github.com/thought-machine/please/archive/v16.21.3.tar.gz"
+  sha256 "cf89880a13cb042f42d7af7482a69c97692ecbb453af5575ecd75376e266b0df"
   bottle do
-    root_url "https://github.com/thought-machine/homebrew-please/releases/download/v16.21.2"
-    sha256 cellar: :any_skip_relocation, el_capitan: "c225021cf36ca882b676ea254b5bede9fa6abc3f540b8168f8b7ac3139b01637"
-    sha256 cellar: :any_skip_relocation, yosemite: "26cd77e22b7b1cbbedf2659b7332e4df9dc0dbf9f9d169e0f6899ed451e9daed"
-    sha256 cellar: :any_skip_relocation, mojave: "32055c1cd31e461e5b973c69152f7fde796feedf1bd0f1282f6486945e4dc211"
-    sha256 cellar: :any_skip_relocation, linux_x86_64: "6a0a2d1c8b4f24fe12c3f4e4abaa471332055c98e05e81786d8f38f26e9dbe62"
+    root_url "https://github.com/thought-machine/homebrew-please/releases/download/v16.21.3"
+    sha256 cellar: :any_skip_relocation, el_capitan: "5a95d46e2e2e79239ce27fe60efab22e860855a9626592396b8d5144eca33d31"
+    sha256 cellar: :any_skip_relocation, yosemite: "765dbf49201355670a90c93575c9c9fc8a8ecee0aa6d065181fc921a0879a1a7"
+    sha256 cellar: :any_skip_relocation, mojave: "59b033180200efb68e306bd133093559f9c20bb5b72e9db33508657b8b9e09e9"
+    sha256 cellar: :any_skip_relocation, linux_x86_64: "3cfc03e6f9ad26c27a99de11b57e24fe58fe575d53334f45e53e81f772607660"
   end
 
   depends_on "go" => :build
